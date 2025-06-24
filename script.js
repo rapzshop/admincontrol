32418b49a234285786aa51dcbb0ab5774aebca4a
rapzshop <rafarasa27@gmail.com>
2025-06-24 08:42:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Firebase Init
 const firebaseConfig = {
   databaseURL: "https://pembayaran-8587d-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
+// Cari Transaksi
 function cariTransaksi() {
   const id = document.getElementById("idTransaksi").value.trim();
   const hasilEl = document.getElementById("hasilTransaksi");
@@ -43,7 +45,7 @@ function konfirmasiTransaksi(id) {
 
     db.ref("pesanan/" + id + "/status").set("selesai").then(() => {
       alert("✅ Transaksi dikonfirmasi.");
-      kirimWA(data);
+      kirimWA(data);  // ⬅ Kirim WA otomatis via Fonnte
       cariTransaksi();
     });
   });
@@ -79,6 +81,7 @@ function tambahStok() {
   });
 }
 
+// Kirim WA via Fonnte (Token Aktif)
 function kirimWA(data) {
   const fonnteToken = "5GMYufEN5CdzTGmwdTn4";
 
